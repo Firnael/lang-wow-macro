ece9ba754eade872480171f949e56a357e70a06d
Audren Burlot <audren.burlot@gmail.com>
2022-09-02 15:00:02 +0200

    ✨ id - added id as numbers + rename identifier to name

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,16 @@
 import { parser } from "./syntax.grammar"
-import { LRLanguage, LanguageSupport, HighlightStyle, indentNodeProp, foldNodeProp, foldInside, delimitedIndent } from "@codemirror/language"
+import { LRLanguage, LanguageSupport, HighlightStyle } from "@codemirror/language"
 import { completeFromList } from "@codemirror/autocomplete"
 import { styleTags, tags as t } from "@lezer/highlight"
-
 
 export const WowMacroLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
-      indentNodeProp.add({
-        Application: delimitedIndent({ closing: ")", align: false })
-      }),
-      foldNodeProp.add({
-        Application: foldInside
-      }),
       styleTags({
         ShowTooltip: t.annotation,
         Function: t.labelName,
-        Identifier: t.variableName,
+        Name: t.variableName,
+        Id: t.number,
         Condition: t.keyword,
         ConditionOperator: t.logicOperator,
         "( )": t.paren,
@@ -31,9 +25,12 @@ export const WowMacroLanguage = LRLanguage.define({
 
 export const WowMacroCompletion = WowMacroLanguage.data.of({
   autocomplete: completeFromList([
-    {label: "/cast", type: "labelName"},
-    {label: "/use", type: "labelName"},
-    {label: "/target", type: "labelName"},
+    { label: '#showtooltip', type: 'annotation' },
+    { label: '/cast', type: 'labelName' },
+    { label: '/use', type: 'labelName' },
+    { label: '/target', type: 'labelName' },
+    { label: '[focus', type: 'keyword' },
+    { label: '[@', type: 'keyword' }
   ])
 })
 
@@ -42,8 +39,9 @@ export const WowMacroHighlightStyle = HighlightStyle.define([
   { tag: t.logicOperator, color: "#ffd000" }, // ';'         => yellow
   { tag: t.annotation, color: "#8fbc8f" },    // ShowTooltip => dark sea green
   { tag: t.labelName, color: "#00bfff" },     // Function    => light blue
-  { tag: t.variableName, color: "#9370db" },  // Identifier  => purple
+  { tag: t.variableName, color: "#9370db" },  // Name        => purple
   { tag: t.keyword, color: "#ff00ff" },       // Condition   => magenta
+  { tag: t.number, color: "#ffa500" },        // Id          => orange
 ])
 
 export function WowMacro() {
