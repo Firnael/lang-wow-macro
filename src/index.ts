29ad1a3bd733bc42fdbd799a190e4de24562e0ba
Audren Burlot <audren.burlot@gmail.com>
2022-09-06 15:15:20 +0200

    ⚗️ autocomplete - trying to complexify the CompletionResult

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { parser } from "./syntax.grammar"
 import { LRLanguage, LanguageSupport, HighlightStyle } from "@codemirror/language"
-import { CompletionContext, completeFromList, CompletionResult } from "@codemirror/autocomplete"
+import { CompletionContext, Completion, CompletionResult } from "@codemirror/autocomplete"
 import { styleTags, tags as t } from "@lezer/highlight"
 
 export const WowMacroLanguage = LRLanguage.define({
@@ -27,16 +27,52 @@ function myCompletions(context: CompletionContext): CompletionResult {
       options: []
     }
   }
-  return {
+
+  const result: CompletionResult = {
     from: word.from,
-    options: [
-      { label: '#showtooltip ', type: 'annotation' },
-      { label: '/cast ', type: 'labelName' },
-      { label: '/use ', type: 'labelName' },
-      { label: '/target ', type: 'labelName' },
-      { label: '@focus', type: 'keyword' },
+    options: []
+  }
+
+  // matches a slash "/", implying we want to use a command
+  if (context.matchBefore(/\//) != null) {
+    result.options = [
+      { label: 'cast ', type: 'labelName' },
+      { label: 'use ', type: 'labelName' },
+      { label: 'target ', type: 'labelName' },
+      { label: 'focus ', type: 'labelName' }
     ]
   }
+  // matches a "#", implying we want to use the "show*" directives
+  else if (context.matchBefore(/#/) != null) {
+    result.options = [
+      { label: 'showtooltip ', type: 'annotation', detail: "Display the ability/item icon & tooltip", info: "If you selected the '?' icon, you can use this annotation to display an icon relative to your macro" },
+      { label: 'show ', type: 'annotation', detail: "Display the ability/item icon" }
+    ]
+  }
+  // matches either a "[" or a "," (with or without a trailing whitespace), implying we want to use a conditional
+  else if (context.matchBefore(/\[|,\s?/) != null) {
+    result.options = [
+      { label: 'dead', type: 'keyword', detail: "Target is dead" },
+      { label: 'harm', type: 'keyword', detail: "Target can be harmed" },
+      { label: 'help', type: 'keyword', detail: "Target can be helped" },
+      { label: 'indoors', type: 'keyword', detail: "You are indoors" },
+      { label: 'outdoors', type: 'keyword', detail: "You are outdoors" },
+      { label: 'exists', type: 'keyword', detail: "You have a target right now" }
+    ]
+  }
+  // matches a "@", implying we want to use the "target=" operator
+  else if (context.matchBefore(/@/) != null) {
+    result.options = [
+      { label: 'cursor', type: 'keyword' },
+      { label: 'mouseover', type: 'keyword' },
+      { label: 'focus', type: 'keyword' },
+      { label: 'player', type: 'keyword' },
+      { label: 'target', type: 'keyword' },
+      { label: 'targettarget', type: 'keyword' }
+    ]
+  }
+
+  return result
 }
 
 export const WowMacroCompletion = WowMacroLanguage.data.of({
