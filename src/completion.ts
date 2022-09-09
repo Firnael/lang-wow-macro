f1d132be1224e9ec814f84cf08d8ce8233bf1dcc
Audren Burlot <audren.burlot@gmail.com>
2022-09-09 12:14:09 +0200

    ✨ conditions - updated conditions, tests & autocompletion

--- src/completion.ts
+++ src/completion.ts
@@ -3,8 +3,11 @@ import { CompletionContext, Completion, CompletionResult } from "@codemirror/aut
 const commands: Completion[] = [
     { label: '/cast ', type: 'labelName', detail: "Cast a spell" },
     { label: '/use ', type: 'labelName', detail: "Use an item from equipment or inventory" },
-    { label: '/target ', type: 'labelName', detail: "Select a target" },
-    { label: '/focus ', type: 'labelName', detail: "Focus a target" }
+    { label: '/target ', type: 'labelName', detail: "Target the specified unit" },
+    { label: '/focus ', type: 'labelName', detail: "Focus a target" },
+    { label: '/stopcasting ', type: 'labelName', detail: "Cancel any cast currently happening" },
+    { label: '/assist ', type: 'labelName', detail: "Targets your target's target :)" },
+    { label: '/cleartarget ', type: 'labelName', detail: "Leaves you with no target" }
 ]
 
 const annotations: Completion[] = [
@@ -20,13 +23,15 @@ const annotations: Completion[] = [
 ]
 
 const conditions: Completion[] = [
+    { label: '@', type: 'keyword', detail: "Target operator (short for 'target=')" },
+    { label: 'target=', type: 'keyword', detail: "Target operator" },
     { label: 'dead', type: 'keyword', detail: "Target is dead" },
     { label: 'harm', type: 'keyword', detail: "Target can be harmed" },
     { label: 'help', type: 'keyword', detail: "Target can be helped" },
     { label: 'indoors', type: 'keyword', detail: "You are indoors" },
     { label: 'outdoors', type: 'keyword', detail: "You are outdoors" },
     { label: 'exists', type: 'keyword', detail: "You have a target right now" }
-]
+].flatMap(i => [{ ...i, label: `[${i.label}` }, { ...i, label: `,${i.label}` }, { ...i, label: `, ${i.label}` }])
 
 const targets: Completion[] = [
     { label: '@cursor', type: 'keyword', detail: "Used with items and spells using a reticule" },
@@ -38,35 +43,50 @@ const targets: Completion[] = [
 ]
 
 export function completions(context: CompletionContext): CompletionResult {
-    let word = context.matchBefore(/\S*/)
-    if (word == null || (word.from == word.to && !context.explicit)) {
-        return { from: 0, options: [] }
-    }
-
     const result: CompletionResult = {
-        from: word.from,
+        from: 0,
         options: []
     }
 
+    // check what "word" we are currently writing
+    const word = context.matchBefore(/\S*/)
+    if (word == null || (word.from == word.to && !context.explicit)) {
+        return result
+    }
+
     // matches a slash "/", implying we want to use a command
-    if (context.matchBefore(/\//) != null) {
+    if (getFirstCharacterOfWord(word) == '/') {
+        result.from = word.from
         result.options = commands
     }
     // matches a "#", implying we want to use the "show*" directives
-    else if (context.matchBefore(/#/) != null) {
+    else if (getFirstCharacterOfWord(word) == '#') {
+        result.from = word.from
         result.options = annotations
     }
     // matches either a "[" or a "," (with or without a trailing whitespace), implying we want to use a conditional
-    else if (context.matchBefore(/\[|,\s?/) != null) {
+    else if (getFirstCharacterOfWord(word) == '[' || getFirstCharacterOfWord(word) == ',') {
+        result.from = word.from
         result.options = conditions
     }
-    // matches a "@", implying we want to use the "target=" operator
-    else if (context.matchBefore(/@/) != null) {
+    // matches a "@" or "target=", implying we want to use the "target" conditional operator
+    else if (getFirstCharacterOfWord(word) == '@') {
+        result.from = word.from
         result.options = targets
     }
-    // else {
-    //     result.options = new Array().concat(commands, annotations, conditions, targets)
-    // }
 
     return result
+}
+
+function getFirstCharacterOfWord(word: Word | null) {
+    if (word) {
+        return word.text.substring(0, 1)
+    }
+    return 0
+}
+
+interface Word {
+    from: number;
+    to: number;
+    text: string;
 }
