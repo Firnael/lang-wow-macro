8afac1ceb64ac00172dbb1a776a2ce50d46b436f
Audren Burlot <audren.burlot@gmail.com>
2022-09-12 15:01:56 +0200

    ⚗️ completion - trying stuff with regex

--- src/completion.ts
+++ src/completion.ts
@@ -31,7 +31,7 @@ const conditions: Completion[] = [
     { label: 'indoors', type: 'keyword', detail: "You are indoors" },
     { label: 'outdoors', type: 'keyword', detail: "You are outdoors" },
     { label: 'exists', type: 'keyword', detail: "You have a target right now" }
-].flatMap(i => [{ ...i, label: `[${i.label}` }, { ...i, label: `,${i.label}` }, { ...i, label: `, ${i.label}` }])
+]//.flatMap(i => [{ ...i, label: `[${i.label}` }, { ...i, label: `,${i.label}` }, { ...i, label: `, ${i.label}` }])
 
 const targets: Completion[] = [
     { label: '@cursor', type: 'keyword', detail: "Used with items and spells using a reticule" },
@@ -60,29 +60,53 @@ export function completions(context: CompletionContext): CompletionResult {
         result.options = commands
     }
     // matches a "#", implying we want to use the "show*" directives
-    else if (getFirstCharacterOfWord(word) == '#') {
+    if (getFirstCharacterOfWord(word) == '#') {
         result.from = word.from
         result.options = annotations
     }
     // matches either a "[" or a "," (with or without a trailing whitespace), implying we want to use a conditional
-    else if (getFirstCharacterOfWord(word) == '[' || getFirstCharacterOfWord(word) == ',') {
-        result.from = word.from
+    if (getFirstCharacterOfWord(word) == '[') {
+        result.from = word.from + 1
         result.options = conditions
     }
+    else if(word.text.indexOf(',') >= 0 || word.text.indexOf(', ') >= 0) {
+        let index = word.text.lastIndexOf(', ') // attention au +2 ici ?
+        if(index < 0) {
+            index = word.text.lastIndexOf(',')
+        }
+        result.from = word.from + index + 1
+        result.options = conditions
+    }
+
     // matches a "@" or "target=", implying we want to use the "target" conditional operator
-    else if (getFirstCharacterOfWord(word) == '@') {
-        result.from = word.from
+    //else if (getFirstCharacterOfWord(word) == '@' || wordContains(word, '[@') || wordContains(word, ',@') || wordContains(word, ', @')) {
+    if (wordMatches(word, new RegExp(/^(@|\[@|,@|, @)/))) {
+        const index = word.text.lastIndexOf('@')
+        result.from = word.from + index
         result.options = targets
     }
+
+    // en vrai le "from" il faut que ça soit à partir du dernier caractère qui nous intéresse
+    // en gros si on a le word "[@cursor,nodead" et qu'on ajoute une ',' le from doit commencer à la dernière ','
+    // donc un word.text.lastIndexOf(',')
 
     return result
 }
 
-function getFirstCharacterOfWord(word: Word | null) {
-    if (word) {
-        return word.text.substring(0, 1)
-    }
-    return 0
+function getFirstCharacterOfWord(word: Word | null): string {
+    return word ? word.text.substring(0, 1) : ''
+}
+
+function getLastCharacterOfWord(word: Word | null): string {
+    return word ? word.text.substring(word.text.length-2, word.text.length-1) : ''
+}
+
+function wordContains(word: Word | null, content: string): boolean {
+    return word ? (word.text.indexOf(content) >=0 ? true : false) : false
+}
+
+function wordMatches(word: Word | null, match: RegExp): boolean {
+    return word ? match.test(word.text) : false
 }
 
 interface Word {
